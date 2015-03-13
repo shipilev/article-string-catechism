c9acc658718500dd460afe8c04251551da7c26f4
Aleksey Shipilev <aleksey.shipilev@gmail.com>
2015-03-13 11:12:58 +0300

    Tuneups.

--- src/main/java/org/openjdk/util/DigramGen.java
+++ src/main/java/org/openjdk/util/DigramGen.java
@@ -14,51 +14,54 @@ public class DigramGen {
     public static void main(String... args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in, Charset.forName("UTF-8")));
 
-        List<String> lines = new ArrayList<>();
+        List<String> trimmed = new ArrayList<>();
         String line;
         while ((line = br.readLine()) != null) {
             String trim = line.trim();
             if (!trim.isEmpty()) {
-                lines.add(trim);
+                trimmed.add(trim);
             }
         }
 
-        int[] mult = new int[lines.size()];
-        int[] hashcode = new int[lines.size()];
+        String[] words = trimmed.toArray(new String[0]);
+        int size = words.length;
+        int[] mult = new int[size];
+        int[] hc = new int[size];
 
-        for (int c = 0; c < lines.size(); c++) {
-            hashcode[c] = lines.get(c).hashCode();
-            mult[c] = powers[lines.get(c).length()];
+        for (int c = 0; c < size; c++) {
+            hc[c] = words[c].hashCode();
+            mult[c] = POWERS[words[c].length()];
         }
 
-        int size = lines.size();
         System.out.println("Read " + size + " words");
 
         int eHC = " ".hashCode();
 
-        IntStream.range(0, lines.size()).parallel().forEach(c1 -> {
-            int hc1 = hashcode[c1] * 31 + eHC;
+        IntStream.range(0, size).parallel().forEach(c1 -> {
+            int hc1 = hc[c1] * 31 + eHC;
             for (int c2 = 0; c2 < size; c2++) {
-                if (hc1 * mult[c2] + hashcode[c2] == 0) {
-                    String s = lines.get(c1) + " " + lines.get(c2);
-                    System.out.printf("%8.5f%%, hit for \"%s\", hc = %d%n", 100.0 * processed.get() / size, s, s.hashCode());
+                if ((hc1 * mult[c2] + hc[c2]) == TARGET_HASHCODE) {
+                    String s = words[c1] + " " + words[c2];
+                    System.out.printf("%8.5f%%, hit for \"%s\", hc = %d%n", 100.0 * PROCESSED.get() / size, s, s.hashCode());
                 }
             }
-            processed.incrementAndGet();
+            PROCESSED.incrementAndGet();
         });
     }
 
-    static AtomicInteger processed = new AtomicInteger();
+    static final int TARGET_HASHCODE = Integer.getInteger("targetHC", 0);
 
-    static int[] powers;
+    static final AtomicInteger PROCESSED = new AtomicInteger();
+
+    static final int[] POWERS;
 
     static {
-        final int POWERS = 10000;
-        powers = new int[POWERS];
+        final int MAX = 10000;
+        POWERS = new int[MAX];
 
         int r = 1;
-        for (int c = 0; c < POWERS; c++) {
-            powers[c] = r;
+        for (int c = 0; c < MAX; c++) {
+            POWERS[c] = r;
             r *= 31;
         }
     }
